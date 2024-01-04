979fb9b7b5f78ef82d4aab49974db1830fc23012
Yurii <zumbala2013@gmail.com>
2024-01-04 19:31:07 +0200

    Setup basic navigation & layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: DashboardComponent },
+  { path: "add", component: AddBookComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { AddBoockComponent } from './components/add-boock/add-boock.component';
 import { AddBookComponent } from './components/add-book/add-book.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    AddBoockComponent,
     AddBookComponent
   ],
   imports: [
