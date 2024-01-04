a1f6b92b8e83260af0504972ec564392cff9643e
Yurii <zumbala2013@gmail.com>
2024-01-04 18:55:56 +0200

    Add main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AddBoockComponent } from './components/add-boock/add-boock.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AddBoockComponent,
+    AddBookComponent
   ],
   imports: [
     BrowserModule,
